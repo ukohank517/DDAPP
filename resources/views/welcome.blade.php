0074510112a95deabf7878f93bae11ae99510ac2
User Purpleotter7 <purpleotter7@www3837.sakura.ne.jp>
2018-08-25 09:58:33 +0900

    [update] DDApp用ホームページ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>DDApp</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -79,9 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    DDApp
                 </div>
 
+		<!--
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
@@ -90,6 +91,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+		-->
             </div>
         </div>
     </body>
