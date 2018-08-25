cb7df956f3b1037d4e0803c822e8543d957c6a51
ukohank517 <ukohank517@gmail.com>
2018-08-25 11:08:22 +0900

    [add] ユーザー登録部分ののuserid追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,6 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+	    'userid' => $data['userid'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
