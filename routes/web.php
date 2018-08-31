c7b258237a4b00b2718cad7ec635ccf366876e46
ukohank517 <ukohank517@gmail.com>
2018-08-31 11:48:21 +0900

    [add] 制限ルーティングの例

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,29 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+/*
+// 全ユーザー
+Route::group(['middleware'=>['auth', 'can:user-higher']], function(){
+    // ユーザ一覧
+    Route::get('/account', 'AccountController@index')->name('account.index');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+// 管理者以上
+Route::group(['middleware' => ['auth', 'can:admin-higher']], function(){
+    // ユーザ登録
+    Route::get('/account/regist', 'AccountController@regist')->name('account.regist');
+    Route::post('/account/regist', 'AccountController@createData')->name('account.regist');
+
+    // ユーザ編集
+    Route::get('/account/edit/{user_id}', 'AccountController@edit')->name('account.edit');
+    Route::post('/account/edit/{user_id}', 'AccountController@edit')->name('account.edit');
+});
+
+
+Route::group(['middleware' => ['auth', 'can:system-only']],function(){
+    // ユーザ削除
+    Route::post('/account/delete/{user_id}', 'AccountController@deleteData');
+});
+*/
