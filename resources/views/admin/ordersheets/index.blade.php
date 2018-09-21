de41b30b8feb5f258163434c68c145867a2dfff2
ukohank517 <ukohank517@gmail.com>
2018-09-21 11:34:39 +0900

    [add] 業務DB

---
+++ resources/views/admin/ordersheets/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading"><font size="7">orderSheet管理ページ</font></div>
+		<div class="panel-body">
+                  @if (Session::has('flash_message'))
+                    <div class="alert alert-success">{{ Session::get('flash_message') }}</div>
+                  @endif
+
+                    {{-- エラーの表示 --}}
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="mb10">
+                        {!! Form::model($ordersheets, [
+                            'url' => route('admin::ordersheets.upload'),
+                            'method' => 'POST',
+                            'files' => true
+                        ]) !!}
+                        <div class="row">
+                            <div class="col-md-4">
+                                {!! Form::file('csv_file', null, ['class' => 'form-control']) !!}
+                            </div>
+                            <div class="col-md-8">
+                                {!! Form::submit('下に追加', ['class' => 'btn btn-primary']) !!}
+                                {{ link_to_route('admin::ordersheets.download', 'ダウンロード', null, ['class' => 'btn btn-default']) }}
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
+		    
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead>
+                        <tr>
+			    <th width="15%">id</th>
+                            <th width="15%">date</th>
+                            <th width="15%">box</th>
+			    <th width="15%">sku</th>
+			    <th width="15%">line</th>
+			    <th width="15%">名前</th>
+                        </tr>
+                        </thead>
+			@foreach($ordersheets as $item)
+                            <tr>
+			        <td>{{ $item->id }}</td>
+                                <td>{{ $item->date }}</td>
+                                <td>{{ $item->box }}</td>
+                                <td>{{ $item->sku }}</td>
+				<td>{{ $item->line }}</td>
+				<td>{{ $item->customer_name }}</td>
+                            </tr>
+			@endforeach
+			    {{$ordersheets->links()}}<!-- ページバーを表示する。 -->
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
