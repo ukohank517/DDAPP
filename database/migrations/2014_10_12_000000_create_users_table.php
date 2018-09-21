2f60c7f4b6951aa6c0fbc8101f3b662b6dc9379b
ukohank517 <ukohank517@gmail.com>
2018-09-21 16:27:34 +0900

    [add] user処理ボックス

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+	    $table->integer('dealing_box_name')->default(0)->comment('処理しているボックス名');
             $table->rememberToken();
             $table->timestamps();
         });
