f55c2d334bf4c058bbaea0bd52b95a53f373fb2c
ukohank517 <ukohank517@gmail.com>
2019-03-14 13:17:26 +0900

    [update] 提案と処理は同じ動作なので、ルート書き換え

--- routes/web.php
+++ routes/web.php
@@ -89,5 +89,5 @@ Route::group(['prefix' => 'order_receive', 'middleware'=>['auth', 'can:user-high
 
 Route::group(['prefix' => 'stock_work', 'middleware'=>['auth', 'can:user-higher'], 'namespace' => 'Stock_work', 'as' => 'stock_work::'], function(){
     Route::get('work', 'WorkController@index')->name('work');
-    Route::get('work/recommend', 'WorkController@recommend')->name('work.recommend');
+    Route::get('work/deal_and_recommend', 'WorkController@deal_and_recommend')->name('work.deal_and_recommend');
 });
